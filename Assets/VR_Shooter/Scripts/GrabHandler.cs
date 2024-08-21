1886ff4bf2bfb235222ab01c69aeb78f311a77da
JamesCharnley <james.anthony.charnley@gmail.com>
2024-08-21 19:56:55 +1200

    Update GrabHandler.cs

--- Assets/VR_Shooter/Scripts/GrabHandler.cs
+++ Assets/VR_Shooter/Scripts/GrabHandler.cs
@@ -29,6 +29,7 @@ public class GrabHandler : MonoBehaviour
         {
             grabbedObject.ReleaseFromHand();
             OnReleasedObject?.Invoke();
+            grabbedObject = null;
         }
     }
 
